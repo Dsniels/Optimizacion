6fdbc48501847beebf004e8c2386812a5f0df8aa
Daniel Salazar <dsnile.salazr@gmail.com>
2024-10-13 19:08:31 -0600

    algoritmo del vecino mas cercano

---
+++ vecinoMasCercano.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include "vecinoMasCercano.h"
+
+
+using namespace std;
+
+extern int n;
+extern int distancia;
+
+vector<int> vecinoMasCercano(const vector<vector<double>>& matriz, int inicio, int n) {
+    vector<int> tour;
+    vector<bool> visitado(n, false);
+    int nodoActual = inicio;
+    tour.push_back(nodoActual);
+    visitado[nodoActual] = true;
+
+    for (int i = 0; i < n - 1; ++i) {
+        double minDistance = INT32_MAX;
+        int next = -1;
+
+        for (int j = 0; j < n; ++j) {
+            if (!visitado[j] && matriz[nodoActual][j] < minDistance) {
+                minDistance = matriz[nodoActual][j];
+                next = j;
+            }
+        }
+
+        if (next != -1) {
+            distancia += minDistance;
+            tour.push_back(next);
+            visitado[next] = true;
+            nodoActual = next;
+        }
+    }
+    distancia += matriz[nodoActual][inicio];
+    tour.push_back(inicio);
+    cout<< endl<<"Distancia Recorrida: "<<distancia<<endl;
+
+    return tour;
+}
---
+++ vecinoMasCercano.h
@@ -0,0 +1,9 @@
+#ifndef VECINO_MAS_CERCANO
+#define VECINO_MAS_CERCANO
+
+
+#include <vector>
+
+std::vector<int> vecinoMasCercano(const std::vector<std::vector<double>>&matriz, int incio, int N);
+
+#endif
