ac37b7d4e61aefd4a7a6d2ec6304c71420bbb660
Daniel Salazar <dsnile.salazr@gmail.com>
2024-10-13 19:58:36 -0600

    traducir txt a matriz c++

---
+++ matrices/leer.py
@@ -0,0 +1,29 @@
+def leer_matriz_desde_txt(ruta_archivo):
+    matriz = []
+
+    with open(ruta_archivo, 'r') as archivo:
+        lineas = archivo.readlines()
+        # Saltar la primera línea con el número de elementos
+        for linea in lineas[2:]:
+            # Dividir la línea en valores separados por espacio y convertir a float
+            valores = list(map(float, linea.split()[1:]))  # Ignorar el primer valor (índice)
+            matriz.append(valores)
+
+    return matriz
+
+def convertir_a_formato_cpp(matriz):
+    formato_cpp = "std::vector<std::vector<double>> matriz = {\n"
+    for fila in matriz:
+        formato_cpp += "    {" + ", ".join(map(str, fila)) + "},\n"
+    formato_cpp += "};"
+    return formato_cpp
+
+# Ruta del archivo que contiene la matriz
+ruta_archivo = 'matriz72.txt'
+
+# Leer la matriz desde el archivo y convertirla al formato C++
+matriz = leer_matriz_desde_txt(ruta_archivo)
+formato_cpp = convertir_a_formato_cpp(matriz)
+
+# Mostrar la matriz en formato C++
+print(formato_cpp)
