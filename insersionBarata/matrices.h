63e69054442680bf02b46836c9b530dc221207cb
Daniel Salazar <dsnile.salazr@gmail.com>
2024-10-18 23:52:57 -0600

    definicion e implementacion de matrices

---
+++ insersionBarata/matrices.h
@@ -0,0 +1,18 @@
+
+#ifndef MATRICES
+#define MATRICES
+
+#include<vector>
+using namespace std;
+
+
+extern vector<vector<double>>matriz10;
+extern vector<vector<double>>matriz52;
+extern vector<vector<double>>matriz76;
+
+#endif
+
+
+
+
+
