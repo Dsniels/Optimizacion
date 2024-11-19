7d675ac4a14e998844ec31fc692a6c3a3ac147f8
Daniel Salazar <dsnile.salazr@gmail.com>
2024-11-19 00:07:27 -0600

    Agregar precisión al tiempo computacional y mejorar la legibilidad en la salida de consola

--- PIA MTVRP/main.cpp
+++ PIA MTVRP/main.cpp
@@ -3,9 +3,8 @@
 #include <algorithm>
 #include "ruteo.h"
 #include "matrices.h"
-
+#include <iomanip>
 using namespace std;
-
 
 int main()
 {
@@ -14,6 +13,7 @@ int main()
     MTVRP mtvrp(matriz10, 120, 2, 10, demandaClientes);
 
     clock_t compTime = clock();
+    vector<int> service_time(10, 0);
 
     vector<vector<int>> rutas = mtvrp.getRutas();
     int tiempoTotal = mtvrp.getTiempo();
@@ -25,8 +25,8 @@ int main()
         for_each(ruta.begin(), ruta.end(), printV);
         cout<<endl;
     }
-
-    cout<< "Tiempo computacional: "<<(float)compTime/CLOCKS_PER_SEC<<endl;
+    cout<<fixed << setprecision(6);
+    cout<< "Tiempo computacional: "<<(float)compTime/CLOCKS_PER_SEC<<" Segs"<<endl;
     cout << "Tiempo Total de Espera: " << tiempoTotal << endl;
     return 0;
 }
