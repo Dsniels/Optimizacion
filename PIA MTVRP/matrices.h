5b44d0712e421fd625452450ed60e8c8bc8abf27
Daniel Salazar <dsnile.salazr@gmail.com>
2024-11-23 01:22:40 -0600

    Agregar nuevas variables de demanda y distancia para diferentes escenarios en matrices.h

--- PIA MTVRP/matrices.h
+++ PIA MTVRP/matrices.h
@@ -5,6 +5,28 @@
 
 using namespace std;
 
-extern vector<int> demandaClientes;
-extern vector<vector<double>> matriz10;
+extern vector<int> vector10;
+
+extern vector<int> s0_01_Demandas;
+extern vector<vector<double>>s0_01_Distancias;
+extern vector<int> s0_05_Demandas;
+extern vector<vector<double>> s0_05_Distancias;
+extern vector<int> s0_03_Demandas;
+extern vector<vector<double>> s0_03_Distancias;
+extern vector<int> s0_04_Demandas;
+extern vector<vector<double>> s0_04_Distancias;
+extern vector<int> VRPNC1_Demandas;
+extern vector<vector<double>> VRPNC1_Distancias;
+extern vector<int> VRPNC3_Demandas;
+extern vector<vector<double>> VRPNC3_Distancias;
+extern vector<int> VRPNC2_Demandas;
+extern vector<vector<double>> VRPNC2_Distancias;
+extern vector<int> VRPNC12_Demandas;
+extern vector<vector<double>> VRPNC12_Distancias;
+extern vector<int> VRPNC4_Demandas;
+extern vector<vector<double>> VRPNC4_Distancias;
+// extern vector<int> VRPNC5_Demandas;
+// extern vector<vector<double>> VRPNC5_Distancias;
+// extern vector<int> VRPNC11_Demandas;
+// extern vector<vector<double>> VRPNC11_Distancias;
 #endif
