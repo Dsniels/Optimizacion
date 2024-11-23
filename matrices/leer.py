3d4064bb91ce1ca798befcf7a0b248c82e0f4f75
Daniel Salazar <dsnile.salazr@gmail.com>
2024-11-23 01:22:26 -0600

    Actualizar configuraciones de asociación de archivos en Visual Studio Code y modificar la función de lectura de matrices para mejorar la legibilidad y el formato de salida.

--- matrices/leer.py
+++ matrices/leer.py
@@ -4,22 +4,22 @@ def leer_matriz_desde_txt(ruta_archivo):
     with open(ruta_archivo, 'r') as archivo:
         lineas = archivo.readlines()
         # Saltar la primera línea con el número de elementos
-        for linea in lineas[2:]:
+        for linea in lineas:
             # Dividir la línea en valores separados por espacio y convertir a float
-            valores = list(map(float, linea.split()[1:]))  # Ignorar el primer valor (índice)
+            valores = list(map(float, linea.split()))  # Ignorar el primer valor (índice)
             matriz.append(valores)
 
     return matriz
 
 def convertir_a_formato_cpp(matriz):
-    formato_cpp = "vector<vector<double>> matriz76 = {\n"
+    formato_cpp = "vector<vector<double>> VRPNC2 = {\n"
     for fila in matriz:
         formato_cpp += "    {" + ", ".join(map(str, fila)) + "},\n"
     formato_cpp += "};"
     return formato_cpp
 
 # Ruta del archivo que contiene la matriz
-ruta_archivo = 'd:/Repositorios/Optimizacion/matrices/matrix76.txt'
+ruta_archivo = 'd:/Repositorios/Optimizacion/matrices/2m.txt'
 
 # Leer la matriz desde el archivo y convertirla al formato C++
 matriz = leer_matriz_desde_txt(ruta_archivo)
